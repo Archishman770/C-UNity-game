130e0af5e2a6f66717023eda4409a222e67e26e7
Archishman770 <archishman770@gmail.com>
2025-03-29 11:52:32 -0500

    Add SurvivalGame/Assets/Scripts/Game/TestSceneSetup.cs 1743267151819

---
+++ SurvivalGame/Assets/Scripts/Game/TestSceneSetup.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class TestSceneSetup : MonoBehaviour
+{
+    [Header("Player")]
+    public GameObject playerPrefab;
+    public Vector2 playerSpawnPosition = new Vector2(0, 0);
+
+    [Header("Boss")] 
+    public GameObject bossPrefab;
+    public Vector2 bossSpawnPosition = new Vector2(5, 0);
+
+    [Header("Camera")]
+    public float cameraSize = 5f;
+    public Vector3 cameraOffset = new Vector3(0, 0, -10);
+
+    void Start()
+    {
+        // Spawn player
+        GameObject player = Instantiate(
+            playerPrefab, 
+            playerSpawnPosition, 
+            Quaternion.identity
+        );
+        player.tag = "Player";
+
+        // Spawn boss
+        Instantiate(
+            bossPrefab, 
+            bossSpawnPosition, 
+            Quaternion.identity
+        );
+
+        // Camera setup
+        Camera.main.orthographic = true;
+        Camera.main.orthographicSize = cameraSize;
+        Camera.main.transform.position = new Vector3(
+            playerSpawnPosition.x + cameraOffset.x,
+            playerSpawnPosition.y + cameraOffset.y,
+            cameraOffset.z
+        );
+
+        // Set sorting layers
+        if (playerPrefab != null)
+        {
+            playerPrefab.GetComponent<SpriteRenderer>().sortingLayerName = "Characters";
+        }
+        if (bossPrefab != null) 
+        {
+            bossPrefab.GetComponent<SpriteRenderer>().sortingLayerName = "Characters";
+        }
+    }
+}
